d5d4457091ddc6d33885d3ae64238bf70831085e
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-07-15 21:06:09 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -11,3 +11,52 @@ int isLessThanThousand(const char *value){
     int input = atoi(value); // string to int
     return input < 1000 ? sum += input : 0+sum;
 }
+
+int calculateSum(const char* input, char* delimiter)
+{
+    int sum = 0;
+    char* duplicate_input = NULL;
+    duplicate_input = strdup (input); // duplicate the string 
+    char* token = strtok(duplicate_input, delimiter); //tokenizing strings
+    while(token != NULL)
+    {
+        isLessThanThousand(token);
+        token = strtok(NULL,delimiter);
+    }
+    return isLessThanThousand(token);
+}
+
+void appendCustomDelimiter(const char* input, char* delimiter) {
+    int i = 2; 
+    while(input[i] != '\n')
+    {
+        char src[1] = {input[i]};
+        strncat (delimiter, src,1);
+        i += 1;
+    }
+}
+
+//ExpectSumWithCustomDelimiter("//;\n1;2")
+void checkCustomDelimiter(const char* input, char* delimiter)
+{
+    if (input[0] == '/' && input[1] == '/'){
+        strcpy(delimiter, "");
+        appendCustomDelimiter(input,delimiter);
+    }
+    else{
+        delimiter = ",\n";
+    }
+}
+
+//ExpectSumWithCustomDelimiter
+int add(const char* input)
+{
+    char delimiter[128] = ",\n";
+    if (1 == isEmptyString(input))
+    {
+        return 0;
+    }
+    checkCustomDelimiter(input, delimiter);
+    return calculateSum(input, delimiter);
+    
+}
