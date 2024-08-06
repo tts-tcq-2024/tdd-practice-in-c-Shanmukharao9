d961258264e373bd924556552549b909739a0c85
Shanmukharao9 <167960025+Shanmukharao9@users.noreply.github.com>
2024-08-06 07:14:37 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,62 +1,110 @@
-#include <string.h>
-#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
 
-//Check is String Empty or Null  
-bool isEmptyString(const char* value){
-    return (value == NULL || value[0] == '\0');
+/*To find out the delimiter in the input string*/
+int isdelimiter(char character, const char *delimiter) {
+    while (*delimiter != '\0') {
+        if (character == *delimiter) {
+            return 1;
+        }
+        delimiter++;
+    }
+    return 0;
 }
 
-int isLessThanThousand(const char *value){    
-    int sum = 0;
-    int input = atoi(value); // string to int
-    return input < 1000 && input >= 0 ? sum += input : 0;
-}
-
-int calculateSum(const char* input, char* delimiter)
+int maxLimit(int num)
 {
-    int sum = 0;
-    char* Duplicate_inp = NULL;
-    Duplicate_inp = strdup (input); // duplicate the string 
-    char* token = strtok(Duplicate_inp, delimiter); //tokenizing strings
-    while(token != NULL)
-    {
-        isLessThanThousand(token);
-        token = strtok(NULL,delimiter);
-    }
-    return isLessThanThousand(token);
+    return (num>=0 && num<=1000) ? num : 0;
 }
 
-void appendCustomDelimiter(const char* input, char* delimiter) {
-    int i = 2; 
-    while(input[i] != '\n')
-    {
-        char src[1] = {input[i]};
-        strncat (delimiter, src,1);
-        i += 1;
-    }
-}
-
-//ExpectSumWithCustomDelimiter("//;\n1;2")
-void checkCustomDelimiter(const char* input, char* delimiter)
+/* positive single digit logic inorder to reduce the complexity logic for the positive_single_number function*/
+bool positive_single_digit(int count,int set,char *token)
 {
-    if (input[0] == '/' && input[1] == '/'){
-        strcpy(delimiter, "");
-        appendCustomDelimiter(input,delimiter);
-    }
-    else{
-        delimiter = ",\n";
-    }
+    return ((count>0) && set==0 && (!isalpha(*token)));
 }
 
-//ExpectSumWithCustomDelimiter
+bool positive_single_number(int flg,int count,int set,char *token)
+{
+    bool is_singledigit=positive_single_digit(count,set,token);
+    return (!flg  && is_singledigit);
+}
+
+/* positive multiple digits logic inorder to reduce the complexity logic for the positive_multiple_numbers function*/
+bool positive_multiple_digits(int flg,int count,int set,char *token)
+{
+    return ((flg || (set>0)));
+}
+
+bool positive_multiple_numbers(int flg,int set,char *token,int count)
+{
+    bool is_multipledigits=positive_multiple_digits(flg,count,set,token);
+    return (is_multipledigits);
+}
+
+int stringCalculation(char *token,int flg,int count,int set) {
+
+    static int sum=0;
+    int prev=atoi(token);
+    bool pos_mn=positive_multiple_numbers(flg,set,token,count);
+    bool pos_sn=positive_single_number(flg,count,set,token);
+
+    /* Logic for multiple numbers separated by delimiters */
+    if(pos_mn)
+    {
+        prev=maxLimit(prev);
+        sum=sum+prev;
+        return sum;
+    }
+
+    /* Logic for single digit number without delimiter */
+    if(pos_sn)
+    {
+        int result=maxLimit((atoi(token)));
+        return (result);
+    }
+
+    return atoi(token);
+
+}
+
+/*To handle the last character in the input string */
+int lastCharacter(int flg,int set,char *strptr,int count,int result)
+{
+    if (*strptr != '\0') {
+        result=stringCalculation(strptr,flg,count,set);
+        return result;
+    }
+    return result;
+}
+
+/* Splitting the strings into substring logic for calculation */
+int splitString(const char *input, const char *delimiters) {
+    char *copyptr;
+    copyptr = (char *)malloc(strlen(input) + 1);
+    strcpy(copyptr,input);//Copying the input string into copyptr inorder to form substring from the input
+    char *strptr = copyptr;
+    int result, count, set=0;
+    while (*copyptr != '\0') {
+        int isDelimiterFound = isdelimiter(*copyptr, delimiters);
+        if (isDelimiterFound) {
+            set++;
+            // Found a delimiter, terminate the current substring
+            *copyptr='\0';
+            result=stringCalculation(strptr,isDelimiterFound,count,set);
+            strptr = copyptr + 1;  // Update start pointer to next character after delimiter
+        }
+        copyptr++;
+        count++;
+    }
+    result = lastCharacter(delimiters,set,strptr,count,result);
+    return result;
+}
+
+
 int add(const char* input)
 {
-    char delimiter[128] = ",\n";
-    if (1 == isEmptyString(input))
-    {
-        return 0;
-    }
-    checkCustomDelimiter(input, delimiter);
-    return calculateSum(input, delimiter);
-    
+    int result=0;
+    const char delimiters[] = ",;\n[]*//";
+    return splitString(input, delimiters);;
 }
